e5cf459b0eb87c5cf01abce8d27e705c66ce1037
beckman924 <braiancarabajal@gmail.com>
2022-01-28 17:39:18 -0300

    Starting activity 1

---
+++ arturito/src/components/Starships/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Starships() {
+  return (
+    <div>
+      <p>
+        Agregar tabla con las starships sacadas de la API. Mostrar para cada
+        starship: name, model, manufacturer, passengers, cantidad de films.
+        Codear en un componente aparte tal como {'<Planets>'}.
+      </p>
+    </div>
+  );
+}
+
+export default Starships;
